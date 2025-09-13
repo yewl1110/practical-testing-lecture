d94aadd3828dd28da01dd4a8861af276eccc2e46
yewl1110 <yewl1110@naver.com>
2025-09-13 20:49:08 +0900

    19. Persistence Layer 테스트(1)

---
+++ src/main/java/sample/cafekiosk/spring/api/service/product/response/ProductResponse.java
@@ -0,0 +1,30 @@
+package sample.cafekiosk.spring.api.service.product.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+@Getter
+@Builder
+public class ProductResponse {
+
+    private Long id;
+    private String productNumber;
+    private ProductType type;
+    private ProductSellingStatus sellingStatus;
+    private String name;
+    private int price;
+
+    public static ProductResponse of(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .productNumber(product.getProductNumber())
+                .type(product.getType())
+                .sellingStatus(product.getSellingStatus())
+                .name(product.getName())
+                .price(product.getPrice())
+                .build();
+    }
+}
---
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductSellingStatus.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.spring.domain.product;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductSellingStatus {
+
+    SELLING("판매 중"),
+    HOLD("판매 보류"),
+    STOP_SELLING("판매 중지"),
+    ;
+
+    private final String description;
+
+    public static List<ProductSellingStatus> forDisplay() {
+        return List.of(SELLING, HOLD);
+    }
+}
